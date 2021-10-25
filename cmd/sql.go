d70e25b851b0e5fc845e678764476fc000a1a9ef
rubiconliu <rubiconliu@tencent.com>
2021-10-26 00:23:09 +0800

    增加sql转换struct的命令

---
+++ cmd/sql.go
@@ -0,0 +1,65 @@
+package cmd
+
+import (
+	"log"
+	"rubicon-cli-tools/internal/sql2struct"
+
+	"github.com/spf13/cobra"
+)
+
+var username string
+var password string
+var host string
+var charset string
+var dbType string
+var dbName string
+var tableName string
+
+var sqlCmd = &cobra.Command{
+	Use:   "sql",
+	Short: "sql转换和处理",
+	Long:  "sql转换和处理",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+var sql2structCmd = &cobra.Command{
+	Use:   "struct",
+	Short: "sql转换为struct",
+	Long:  "sql转换为struct",
+	Run: func(cmd *cobra.Command, args []string) {
+		dbInfo := &sql2struct.DBInfo{
+			DBType:  dbType,
+			Host:    host,
+			User:    username,
+			Passwd:  password,
+			Charset: charset,
+		}
+		dbModel := sql2struct.NewDBModel(dbInfo)
+		err := dbModel.Connect()
+		if err != nil {
+			log.Fatalf("dbModel.Connect err: %v", err)
+		}
+		columns, err := dbModel.GetColumns(dbName, tableName)
+		if err != nil {
+			log.Fatalf("dbModel.GetColumns err: %v", err)
+		}
+
+		template := sql2struct.NewStructTemplate()
+		templateColumns := template.AssemblyColumns(columns)
+		err = template.Generate(tableName, templateColumns)
+		if err != nil {
+			log.Fatalf("template.Generate err: %v", err)
+		}
+	},
+}
+
+func init() {
+	sqlCmd.AddCommand(sql2structCmd)
+	sql2structCmd.Flags().StringVarP(&username, "username", "u", "", "请输入数据库的账号")
+	sql2structCmd.Flags().StringVarP(&password, "password", "p", "", "请输入数据库的密码")
+	sql2structCmd.Flags().StringVarP(&host, "host", "h", "127.0.0.1:3306", "请输入数据库的IP和端口")
+	sql2structCmd.Flags().StringVarP(&charset, "charset", "c", "utf8mb4", "请输入数据库的编码")
+	sql2structCmd.Flags().StringVarP(&dbType, "type", "t", "mysql", "请输入数据库实例类型")
+	sql2structCmd.Flags().StringVarP(&dbName, "db", "", "", "请输入数据库名称")
+	sql2structCmd.Flags().StringVarP(&tableName, "table", "", "", "请输入表名称")
+}
---
+++ internal/sql2struct/mysql.go
@@ -0,0 +1,111 @@
+package sql2struct
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type DBModel struct {
+	DBEngine *sql.DB
+	DBInfo   *DBInfo
+}
+
+type DBInfo struct {
+	DBType  string
+	Host    string
+	User    string
+	Passwd  string
+	Charset string
+}
+
+type TableColumn struct {
+	ColumnName    string
+	DataType      string
+	IsNullable    string
+	ColumnKey     string
+	ColumnType    string
+	ColumnComment string
+}
+
+var DBTypeToStructType = map[string]string{
+	"int":        "int32",
+	"tinyint":    "int8",
+	"smallint":   "int",
+	"mediumint":  "int64",
+	"bigint":     "int64",
+	"bit":        "int",
+	"bool":       "bool",
+	"enum":       "string",
+	"set":        "string",
+	"varchar":    "string",
+	"char":       "string",
+	"tinytext":   "string",
+	"mediumtext": "string",
+	"text":       "string",
+	"longtext":   "string",
+	"blob":       "string",
+	"tinyblob":   "string",
+	"mediumblob": "string",
+	"longblob":   "string",
+	"date":       "time.Time",
+	"datetime":   "time.Time",
+	"timestamp":  "time.Time",
+	"time":       "time.Time",
+	"float":      "float64",
+	"double":     "float64",
+}
+
+func NewDBModel(info *DBInfo) *DBModel {
+	return &DBModel{DBInfo: info}
+}
+
+func (m *DBModel) Connect() error {
+	var err error
+	s := "%s:%s@tcp(%s)/information_schema?" +
+		"charset=%s&parseTime=True&loc=Local"
+
+	dsn := fmt.Sprintf(
+		s,
+		m.DBInfo.User,
+		m.DBInfo.Passwd,
+		m.DBInfo.Host,
+		m.DBInfo.Charset,
+	)
+
+	m.DBEngine, err = sql.Open(m.DBInfo.DBType, dsn)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *DBModel) GetColumns(dbName, tableName string) ([]*TableColumn, error) {
+	query := "SELECT COLUMN_NAME, DATA_TYPE, COLUMN_KEY, " +
+		"IS_NULLABLE, COLUMN_TYPE, COLUMN_COMMENT " +
+		"FROM COLUMNS WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ? "
+	rows, err := m.DBEngine.Query(query, dbName, tableName)
+	if err != nil {
+		return nil, err
+	}
+	if rows == nil {
+		return nil, fmt.Errorf("没有数据")
+	}
+	defer rows.Close()
+
+	res := make([]*TableColumn, 0)
+	for rows.Next() {
+		var tColumn TableColumn
+
+		err := rows.Scan(&tColumn.ColumnName, &tColumn.DataType, &tColumn.ColumnKey,
+			&tColumn.IsNullable, &tColumn.ColumnType, &tColumn.ColumnComment)
+		if err != nil {
+			return nil, err
+		}
+
+		res = append(res, &tColumn)
+	}
+
+	return res, nil
+}
